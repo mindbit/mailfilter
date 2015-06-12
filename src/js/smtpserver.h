4d76e5865faf5723d884f5014ea4865e35d151d0
ionutradu <ionut.radu2712@gmail.com>
2015-06-13 00:20:32 +0300

    Add SmtpClient class with its methods (connect, readResponse, sendCommand, sendMessageBody and constructor)

--- src/js/smtpserver.h
+++ src/js/smtpserver.h
@@ -20,6 +20,15 @@ static JSBool header_getValue(JSContext *cx, unsigned argc, jsval *vp);
 static JSBool header_toString(JSContext *cx, unsigned argc, jsval *vp);
 static JSBool header_refold(JSContext *cx, unsigned argc, jsval *vp);
 
+// SmtpClient class methods
+int init_smtp_client_class(JSContext *cx, JSObject *global);
+static int connect_to_address(char *ip, char *port);
+static JSBool smtp_client_construct(JSContext *cx, unsigned argc, jsval *vp);
+static JSBool smtpClient_sendMessageBody(JSContext *cx, unsigned argc, jsval *vp);
+static JSBool smtpClient_sendCommand(JSContext *cx, unsigned argc, jsval *vp);
+static JSBool smtpClient_readResponse(JSContext *cx, unsigned argc, jsval *vp);
+static JSBool smtpClient_connect(JSContext *cx, unsigned argc, jsval *vp);
+
 // Define C stub functions
 #define DEFINE_HANDLER_STUB(name) \
 	static JSBool smtp##name (JSContext *cx, unsigned argc, jsval *vp) { \
