fd8b6062534ae898b802cdcbf73ef1fae9ed3b93
ionutradu <ionut.radu2712@gmail.com>
2015-06-21 20:27:44 +0300

    Add js_create_response to create a SmtpResponse object

--- src/js/js.h
+++ src/js/js.h
@@ -30,6 +30,7 @@ jsval call_js_handler(const char *cmd);
 int js_get_code(jsval v);
 char *js_get_message(jsval v);
 int js_get_disconnect(jsval v);
+jsval js_create_response(jsval *argv);
 
 // SmtpPath class methods
 int add_path_local(jsval *smtpPath, char *local);
