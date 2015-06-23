07a32ba4f4f2327c5e360267af5b809e887a9232
ionutradu <ionut.radu2712@gmail.com>
2015-06-23 23:29:31 +0300

    Add call_js_handler_with_arg definition

--- src/js/js.h
+++ src/js/js.h
@@ -26,6 +26,7 @@ void js_stop(void);
  */
 jsval js_call(const char *obj, const char *func, jsval arg, ...);
 jsval call_js_handler(const char *cmd);
+jsval call_js_handler_with_arg(const char *cmd, char *arg);
 
 // Get response properties
 int js_get_code(jsval v);
