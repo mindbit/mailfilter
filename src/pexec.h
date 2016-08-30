9ea69f4ab0ee7c5f7e99b47dce1f169eb3e2f81a
Radu Rendec <radu.rendec@mindbit.ro>
2016-08-30 22:07:03 +0200

    Fix all compiler warnings
    Signed-off-by: Radu Rendec <radu.rendec@mindbit.ro>

--- src/pexec.h
+++ src/pexec.h
@@ -24,6 +24,8 @@
 #include "smtp_server.h"
 #include "bfd.h"
 
+struct smtp_server_context;
+
 typedef int (*pexec_send_headers_t)(struct smtp_server_context *ctx, bfd_t *fw);
 typedef int (*pexec_result_t)(struct smtp_server_context *ctx, bfd_t *fr, int status);
 
