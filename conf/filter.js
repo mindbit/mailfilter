aaa924f65d0eb864aa5199d37b5899e08ddddb56
Radu Rendec <radu.rendec@mindbit.ro>
2024-04-21 17:01:11 -0400

    conf: Remove obsolete JS handler examples
    This code was added before the first JS integration (using mozjs) to
    serve as an example of what the SMTP server API for JS would look like.
    It has been commented out since the beginning, and now that the JS
    integration is complete (and even ported to a new JS engine), the
    example code is obsolete.
    
    This commit removes the obsolete example code. The parts that store the
    SMTP transaction details into a SQL database are still preserved but
    moved to the actual JS handlers that use the current API. The database
    related code is still commented out because the libjssql integration has
    not been done yet.

--- conf/filter.js
+++ conf/filter.js
@@ -21,77 +21,6 @@ SmtpServer.listenAddress = [["127.0.0.1", 8025]];
 // connection.
 SmtpServer.debugProtocol = true;
 
-/*
-// Provide the initial SMTP greeting that the server will send to the
-// client, as an SmtpStatus object.
-smtpServer.initialGreeting = function () {
-	// Create SMTP client connection to real server
-	this.client = new SmtpClientConnection("127.0.0.1", "25");
-
-	// Create connection to SQL server
-	this.db = DriverManager.getConnection("mailfilter", "mailfilter");
-
-	// Create a new record in smtp_transactions and save remote server address and port
-	var pstmt = this.db.createPreparedStatement("INSERT INTO smtp_transactions (remote_addr, remote_port) VALUES(?,?)");
-	pstmt.setString(1, smtpServer.clientAddress);
-	pstmt.setString(2, smtpServer.clientPort);
-	pstmt.executeUpdate();
-	var res = pstmt.getGeneratedKeys();
-	res.next();
-	this.smtpTransactionId = res.getString(1);
-	pstmt.close();
-
-	// Connect to real server
-	return this.client.connect();
-	// FIXME connect() tb sa intoarca un SmtpStatus care contine exact greeting-ul de la client sau sa arunce o eroare daca nu se poate conecta
-};
-
-// FIXME smtpServer.getEnvelopeSender() intoarce un obiect SmtpPath care modeleaza struct smtp_path din C
-smtpServer.smtpMail = function () {
-	var envelopeSender = this.getEnvelopeSender();
-
-	// Save the envelope sender in the database
-	var pstmt = this.db.createPreparedStatement("UPDATE smtp_transactions SET envelope_sender=? WHERE smtp_transaction_id=?");
-	pstmt.setString(1, this.smtpTransactionId);
-	pstmt.setString(2, envelopeSender);
-	pstmt.executeUpdate();
-	pstmt.close();
-
-	// relay the command to the real server
-	return this.client.smtpMail(envelopeSender);
-};
-
-// FIXME this.getRecipients() intoarce un array de SmtpPath, care modeleaza struct smtp_path din C
-smtpServer.smtpRcpt = function () {
-	var recipients = this.getRecipients();
-	var recipient = recipients[recipients.length - 1];
-
-	// Save the recipient in the database
-	var pstmt = this.db.createPreparedStatement("INSERT INTO smtp_transaction_recipients(smtp_transaction_id, recipient) VALUES (?,?)");
-	pstmt.setString(1, this.smtpTransactionId);
-	pstmt.setString(2, recipient);
-	pstmt.executeUpdate();
-	pstmt.close();
-
-	// relay the command to the real server
-	return this.client.smtpRcpt(recipient);
-};
-
-// smtpServer.smtpData = function () {} FIXME deocamdata nu tb sa facem nimic in plus fata de ce e built-in
-
-smtpServer.messageBody = function () {
-	// FIXME createReceivedHeader() intoarce un obiect care modeleaza un struct im_header si e generat de create_received_hdr()
-	var receivedHeader = this.createReceivedHeader();
-	this.insertHeaderBefore(receivedHeader, "received");
-	this.client.smtpData();
-	// FIXME client.smtpData() ar tb sa intoarca un SmtpStatus pe care sa il si verificam ca e ok
-	return this.client.sendMessage(this.getHeaders(), this.getMessageCache());
-	// FIXME getHeaders() intoarce un array de obiecte care modeleaza headere
-	// getMessageCache() intoarce calea catre fisierul temporar in care e corpul mesajului
-	// client.sendMessage() intoarce SmtpStatus
-};
-*/
-
 SmtpServer.FILTER_ACCEPT = 0;
 SmtpServer.FILTER_REJECT_TEMPORARILY = 1;
 SmtpServer.FILTER_REJECT_PERMANENTLY = 2;
@@ -116,10 +45,30 @@ SmtpServer.prototype.relayCmd = function(cmd, args)
 	return this.smtpClient.readResponse();
 }
 
+// Provide the initial SMTP greeting that the server will send to the
+// client, as a SmtpResponse object.
 SmtpServer.prototype.smtpInit = function()
 {
+	/* TODO
+	// Create connection to SQL server
+	this.db = DriverManager.getConnection("mailfilter", "mailfilter");
+
+	// Create a new record in smtp_transactions and save remote server address and port
+	var pstmt = this.db.createPreparedStatement("INSERT INTO smtp_transactions (remote_addr, remote_port) VALUES(?,?)");
+	pstmt.setString(1, this.remoteAddr);
+	pstmt.setString(2, this.remotePort);
+	pstmt.executeUpdate();
+	var res = pstmt.getGeneratedKeys();
+	res.next();
+	this.smtpTransactionId = res.getString(1);
+	pstmt.close();
+	*/
+
+	// Create a SMTP client object pointing to the real server
 	this.smtpClient = new SmtpClient("127.0.0.1", 25);
+	// Connect to the real server
 	this.smtpClient.connect();
+	// Read the greeting and pass it back to our client
 	return this.smtpClient.readResponse();
 }
 
@@ -135,11 +84,29 @@ SmtpServer.prototype.smtpEhlo = function(hostname)
 
 SmtpServer.prototype.smtpMail = function(path)
 {
+	/* TODO
+	// Save the envelope sender in the database
+	var pstmt = this.db.createPreparedStatement("UPDATE smtp_transactions SET envelope_sender=? WHERE smtp_transaction_id=?");
+	pstmt.setString(1, this.smtpTransactionId);
+	pstmt.setString(2, path.toString());
+	pstmt.executeUpdate();
+	pstmt.close();
+	*/
+
 	return this.relayCmd("MAIL", "FROM: " + path.toString());
 }
 
 SmtpServer.prototype.smtpRcpt = function(path)
 {
+	/* TODO
+	// Save the recipient in the database
+	var pstmt = this.db.createPreparedStatement("INSERT INTO smtp_transaction_recipients(smtp_transaction_id, recipient) VALUES (?,?)");
+	pstmt.setString(1, this.smtpTransactionId);
+	pstmt.setString(2, path.toString());
+	pstmt.executeUpdate();
+	pstmt.close();
+	*/
+
 	return this.relayCmd("RCPT", "TO: " + path.toString());
 }
 
